7ad1ac6868d840485e30162f4a232c7ae798186b
Rebotilwe <rebotilwemokiba@gmail.com>
2025-11-26 16:32:05 +0200

    updated nav

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 import logo from "../../assets/logo.png";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="navbar">
       <div className="logo">
@@ -12,11 +14,38 @@ const Navbar = () => {
           <img src={logo} alt="LeadLink Africa Logo" className="logo-img" />
         </Link>
       </div>
-      <ul className="nav-links">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/services">Services</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
+
+      {/* Hamburger */}
+      <div
+        className={`hamburger ${menuOpen ? "active" : ""}`}
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      <ul className={`nav-links ${menuOpen ? "active" : ""}`}>
+        <li>
+          <Link to="/" onClick={() => setMenuOpen(false)}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/about" onClick={() => setMenuOpen(false)}>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="/services" onClick={() => setMenuOpen(false)}>
+            Services
+          </Link>
+        </li>
+        <li>
+          <Link to="/contact" onClick={() => setMenuOpen(false)}>
+            Contact
+          </Link>
+        </li>
       </ul>
     </nav>
   );
